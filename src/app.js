2f0b783924983b540ce3ff56ad9f34bff2b3cccb
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-10-28 23:39:45 +0200

    added prod mode

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const morgan = require("morgan");
 
 require("dotenv").config({ path: "../.env" });
@@ -12,5 +13,13 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 app.use("/api/v1/", require("./modules"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 module.exports = app;
